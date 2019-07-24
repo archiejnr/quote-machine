e6a454b96a4251a646dcd35008322961f574f842
Archbold <archiboldpatsanza@gmail.com>
2019-07-24 12:09:01 +0300

    prevented repetetive display of quotes

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
    constructor(props){
      super(props)
      this.state={
-       quotes:[{text:'something stupid',author:'Archie',index:0},{text:'something stupidly different',author:'Daniel',index:1},{text:'something stupid',author:'Donovan',index:2}],
+       quotes:[{text:"Don't cry because it's over, smile because it happened",author:"Dr. Seuss",index:0},{text:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",author:"Marilyn Monroe",index:1},{text:"You only live once, but if you do it right, once is enough.",author:"Mae West",index:2},{text:"In three words I can sum up everything I've learned about life: it goes on.",author:"Robert Frost",index:3},{text:"It is better to be hated for what you are than to be loved for what you are not.",author:"Andre Gide",index:4},{text:"Good friends, good books, and a sleepy conscience: this is the ideal life.",author:"Mark Twain",index:5}],
        currentQuote:{
          text:null,
          author:null,
@@ -15,9 +15,10 @@
    }
    //handleClick function to be called when the next button in the DOM is loaded
    handleClick=()=>{
-     let random=Math.floor(Math.random()*3)+0;
-     while(random===this.state.currentQuote.index){
-       random=Math.floor(Math.random()*3)+0;
+     let random=Math.floor(Math.random()*6)+0;
+     let array=this.state.displayed;
+     while(array.includes(random)){
+       random=Math.floor(Math.random()*6)+0;
      }
      let newArr=this.state.displayed.concat([random]);
      this.setState({
@@ -28,7 +29,7 @@
    }
    //this method is automatically fired before react renders
    componentWillMount(){
-     let random =Math.floor(Math.random()*2)+0;
+     let random =Math.floor(Math.random()*6)+0;
      let newArr=this.state.displayed.concat([random]);
      this.setState({
        currentQuote:this.state.quotes[random],
