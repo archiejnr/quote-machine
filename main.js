120c28b9452911a07759adabcc4655563e2a4e1c
Archbold <archiboldpatsanza@gmail.com>
2019-07-24 12:41:34 +0300

    frontend:tweet link and simple edits

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
    constructor(props){
      super(props)
      this.state={
-       quotes:[{text:"Don't cry because it's over, smile because it happened",author:"Dr. Seuss",index:0},{text:"I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.",author:"Marilyn Monroe",index:1},{text:"You only live once, but if you do it right, once is enough.",author:"Mae West",index:2},{text:"In three words I can sum up everything I've learned about life: it goes on.",author:"Robert Frost",index:3},{text:"It is better to be hated for what you are than to be loved for what you are not.",author:"Andre Gide",index:4},{text:"Good friends, good books, and a sleepy conscience: this is the ideal life.",author:"Mark Twain",index:5}],
+       quotes:[{text:"Don't cry because it's over, smile because it happened",author:"Dr. Seuss",index:0},{text:"Everything you can imagine is real.",author:"Pablo Picasso",index:1},{text:"You only live once, but if you do it right, once is enough.",author:"Mae West",index:2},{text:"In three words I can sum up everything I've learned about life: it goes on.",author:"Robert Frost",index:3},{text:"It is better to be hated for what you are than to be loved for what you are not.",author:"Andre Gide",index:4},{text:"Good friends, good books, and a sleepy conscience: this is the ideal life.",author:"Mark Twain",index:5}],
        currentQuote:{
          text:null,
          author:null,
@@ -39,11 +39,11 @@
    }
 
    render(){
-     return (<div id="quote-box" className="bg-light row">
-                  <div id="text" className="col-12">{this.state.currentQuote.text}</div>
-                  <div id="author" className="col-4 offset-8">{this.state.currentQuote.author}</div>
-                  <button id="new-quote" className="btn btn-primary" onClick={this.handleClick}>next</button>
-                  <i id="tweet-quote"></i>
+     return (<div id="quote-box" className="bg-light">
+                  <div className="row"><div id="text" className="col-12"><span>"</span>{this.state.currentQuote.text}<span>"</span></div></div>
+                  <div className="row"><div id="author" className="font-italic col-5 offset-7">{this.state.currentQuote.author}</div></div>
+                  <div className="row mt-3"><button id="new-quote" className="btn btn-primary" onClick={this.handleClick}>next</button>
+                  <a href="twitter.com/intent/tweet"><i  id="tweet-quote" className='fab fa-twitter'></i></a></div>
                 </div>
               )
             }
